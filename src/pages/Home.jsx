40603192bf62b6fd3dedec8da5272194b02b34a4
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-12-18 20:30:17 -0500

    skeleton homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+
+export default function Landing() {
+  return (
+    <div>
+      <div></div>
+      <div>
+          <a href="projects.html">Experience + Projects</a>
+          <a href="#porfolio">Portfolio</a>
+          <a href="#about">About me!</a>
+      </div>
+      <div></div>
+
+      <div>
+        <div>
+          <p id="helloTitle">hello! my name is</p>
+          <h2>Miranda</h2>
+          <div id="blueline"></div>
+          <p id="description">i’m studying <b>computer science</b> and <b>information technology and web sciences</b> at Rensselaer Polytechnic Insititute.</p>
+        </div>
+      </div>
+    </div>
+  );
+}

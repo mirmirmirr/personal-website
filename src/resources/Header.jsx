96ff3a24614535771f56135875b3b62a6e8bd1e9
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-12-19 15:48:45 -0500

    homepage

--- src/resources/Header.jsx
+++ src/resources/Header.jsx
@@ -6,7 +6,7 @@ import linkedinIcon from '../assets/linkedin.png';
 export default function Header() {
   return (
     <div className="m-[30px]">
-      <div className="w-[663px] h-[49px] absolute rounded-[10px] border border-black flex items-center px-4">
+      <div className="w-[663px] h-[5vh] absolute rounded-[10px] border border-black flex items-center px-4">
         <div className="text-black text-[15px] font-normal font-['Poppins'] space-x-2">
           <Link to="/" className="hover:underline">Miranda Zheng</Link> /
           <Link to="/projects" className="hover:underline">Projects</Link> /
---
+++ src/resources/ResizeableBox.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Rnd } from "react-rnd";
+
+export default function ResizableBox({ defaultWidth, defaultHeight, children }) {
+  return (
+    <Rnd
+      default={{
+        x: 100,
+        y: 100,
+        width: defaultWidth,
+        height: defaultHeight,
+      }}
+      minWidth={50}
+      minHeight={50}
+      style={{
+        border: "2px solid #3395ff",
+        borderRadius: "4px",
+        position: "relative",
+      }}
+    >
+    <div className="p-4 flex flex-col items-start justify-center">{children}</div>
+
+      {/* Add draggable corners */}
+      <div className="absolute w-[15px] h-[15px] bg-white border-2 border-[#3395ff]" style={{ top: -8, left: -8 }}></div>
+      <div className="absolute w-[15px] h-[15px] bg-white border-2 border-[#3395ff]" style={{ top: -8, right: -8 }}></div>
+      <div className="absolute w-[15px] h-[15px] bg-white border-2 border-[#3395ff]" style={{ bottom: -8, left: -8 }}></div>
+      <div className="absolute w-[15px] h-[15px] bg-white border-2 border-[#3395ff]" style={{ bottom: -8, right: -8 }}></div>
+    </Rnd>
+  );
+}

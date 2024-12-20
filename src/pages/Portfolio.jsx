6fda33bc61778bf4018a2bef615e1b1575b57453
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2024-12-19 20:00:44 -0500

    portfolio images

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import MasonryExample from "../resources/MasonryExample";
-import Header from "../resources/Header";
+import { GroupCard } from "../resources/Cards"
 
 export default function Portfolio() {
   return (
     <div className="flex flex-col m-8 mt-0">
+        <div>
+            <GroupCard />
+            <GroupCard />
+            <GroupCard />
+        </div>
+
+
         <MasonryExample />
     </div>
   );
---
+++ src/resources/Cards.jsx
@@ -0,0 +1,13 @@
+export function ImageCard() {
+  return (
+    <div>image</div>
+  );
+};
+
+export function GroupCard() {
+  return (
+    <div className="bg-gray-200 p-4 rounded shadow-md"> 
+      nnn
+    </div>
+  ); 
+}

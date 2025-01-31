6dcaa2228f5e2b5b09dacea6813d34e9b8e26e8c
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-01-31 11:57:46 -0500

    About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,10 +9,10 @@ export default function About() {
       />
       <div className="flex flex-col gap-4">
         <p>
-          I'm a junior at Rensselaer Polytechnic Institute, studying Computer Science (CS) and Information Technology and Web Sciences (ITWS). My younger sister actually introduced me to computer science through a high school elective she took, and I was drawn to the field because of the versatility it offered. Later, I added ITWS because I wanted to learn more about the "real-world" applications of technology.
+          Hello! My name is Miranda and I'm a junior at Rensselaer Polytechnic Institute, studying Computer Science (CS) and Information Technology and Web Sciences (ITWS). My younger sister actually introduced me to computer science in high school and I was drawn to the field because of the versatility it offered. Later, I added ITWS because I wanted to learn more about the "real-world" applications of technology.
         </p>
         <p>
-          I've stuck with this path because I found that I enjoy solving programming problems and finding ways to use technology to make life more accessible and simpler. I especially love working on projects that sit at the intersection of development and design—creating experiences that are not only visually appealing but also optimized for performance and usability.
+          I love solving programming problems and finding ways to use technology to make life simpler. I especially love working on projects that sit at the intersection of development and design—creating experiences that are not only visually appealing but also optimized for performance and usability.
         </p>
         <p>
           In my free time, I enjoy spending time with friends, eating, and drawing. Recently, I’ve also started learning about photography and am excited to play around with it!

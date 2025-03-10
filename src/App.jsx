4da56f8bfd641ad651c3c78df72e4ebd29f8580f
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-03-10 11:52:44 -0400

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,7 @@ import Home from './pages/Home.jsx';
 import Portfolio from './pages/Portfolio.jsx';
 import Projects from './pages/Projects.jsx';
 import About from './pages/About.jsx';
-import Resume from './pages/Resume.jsx';
 import Footer from './components/Footer.jsx';
-import Artwork from './pages/creative/Artwork.jsx';
-import Logos from './pages/creative/Logos.jsx';
-import HackRPI from './pages/creative/HackRPI.jsx';
 
 function AppLayout() {
   const location = useLocation();
@@ -32,11 +28,7 @@ function AppLayout() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/portfolio/art" element={<Artwork />} />
-          <Route path="/portfolio/logos" element={<Logos />} />
-          <Route path="/portfolio/hackrpi" element={<HackRPI />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </div>

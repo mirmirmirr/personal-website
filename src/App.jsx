72c2ab91623f7d80582b603db54bfbb16e183fc4
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-03-13 12:45:13 -0400

    replace with react speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 import Home from './pages/Home.jsx';
 import Portfolio from './pages/Portfolio.jsx';

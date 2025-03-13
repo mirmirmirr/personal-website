7697b87f62069612c7ccb24c27744d3ac2eeb0ca
Miranda Zheng <123515762+mirmirmirr@users.noreply.github.com>
2025-03-13 12:43:20 -0400

    add speedInsights and analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import Home from './pages/Home.jsx';
 import Portfolio from './pages/Portfolio.jsx';
 import Projects from './pages/Projects.jsx';
@@ -52,6 +55,8 @@ function App() {
       <ScrollToTop />
       <AppLayout />
       <Footer />
+      <Analytics />
+      <SpeedInsights />
     </Router>
   );
 }
